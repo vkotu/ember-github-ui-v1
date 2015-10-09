2eb486cfe59168492c3ddfb5b6c11cb9359eb1d4
venkat kotu <vikkii89@gmail.com>
2015-10-08 17:08:31 -0700

    changes to route and templates

--- app/router.js
+++ app/router.js
@@ -6,6 +6,18 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', {}, function() {
+    this.route('mike', {});
+  });
+  this.route('orgs', {}); //orgs/
+  this.route('org', {path:'org/:id'}, function() { //org/emberjs
+    this.route('repos', {});
+    this.route('repo', {path:':repoid'}, function() {
+      this.route('contributors', {});
+      this.route('issues', {});
+    }); //org/jquery/jquery-ui
+  });
+  this.route('notfound', {path:'*path'})
 });
 
 export default Router;
---
+++ tests/unit/routes/home/mike-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:home/mike', 'Unit | Route | home/mike', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
